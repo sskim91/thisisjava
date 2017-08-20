2db9f63b1e4cfdd5a3f1a2a8a3f4e1080b30a13c
sskim <tjdtjq91@gmail.com>
2017-08-20 23:20:47 +0900

    예외처리 Exception

--- src/chapter13/WildCardExample.java
+++ src/chapter13/WildCardExample.java
@@ -4,11 +4,22 @@ import java.util.Arrays;
 
 public class WildCardExample {
 
-    public static void registerCourse(Course<?> course) {   //모든 타입
-        System.out.println(course.getName() + " 수강생 " + Arrays.toString(course.getStudents()));
-    }
-
-    public static void registerCourseStudent(Course<? extends Student> course) {
-        System.out.println(course.getName() + " 수강생 " + Arrays.toString(course.getStudents()));
-    }
+//    public static void registerCourse(Course<?> course) {   //모든 타입
+//        System.out.println(course.getName() + " 수강생 " + Arrays.toString(course.getStudents()));
+//    }
+//
+//    public static void registerCourseStudent(Course<? extends Student> course) {
+//        System.out.println(course.getName() + " 수강생 " + Arrays.toString(course.getStudents()));
+//    }
+//
+//    public static void registerCourseWorker(Course<? super Worker> course) {
+//        System.out.println(course.getName() + " 수강생 " + Arrays.toString(course.getStudents()));
+//    }
+//
+//    public static void main(String[] args) {
+//        Course<Person> personCourse = new Course<Person>("일반인과정", 5);
+//        personCourse.add(new Person("일반인"));
+//        personCourse.add(new Worker("직장인"));
+//
+//    }
 }
