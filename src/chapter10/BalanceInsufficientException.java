b4aa14aee5c9811199a7070e52bd4cd2d74fdbc4
sskim <tjdtjq91@gmail.com>
2017-08-22 22:57:37 +0900

    예외 떠넘기기와 사용자정의 예외 만들기

---
+++ src/chapter10/BalanceInsufficientException.java
@@ -0,0 +1,11 @@
+package chapter10;
+
+public class BalanceInsufficientException extends Exception {
+    public BalanceInsufficientException() {
+
+    }
+
+    public BalanceInsufficientException(String message) {
+        super(message);
+    }
+}
