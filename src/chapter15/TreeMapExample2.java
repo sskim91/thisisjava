ad09047c4d8735ccb3211cd49005f95c0bf6f107
sskim <tjdtjq91@gmail.com>
2017-08-23 17:35:25 +0900

    TreeMap 컬렉션

---
+++ src/chapter15/TreeMapExample2.java
@@ -0,0 +1,31 @@
+package chapter15;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class TreeMapExample2 {
+    public static void main(String[] args) {
+        //객체 정렬하기
+        TreeMap<Integer, String> scores = new TreeMap<>();
+        scores.put(new Integer(87), "홍길동");
+        scores.put(new Integer(98), "이성우");
+        scores.put(new Integer(75), "이수용");
+        scores.put(new Integer(95), "허유강");
+        scores.put(new Integer(80), "김성섭");
+
+        NavigableMap<Integer, String> descendingMap = scores.descendingMap();
+        Set<Map.Entry<Integer, String>> descendingEntrySet = descendingMap.entrySet();
+        for (Map.Entry<Integer, String> entry : descendingEntrySet) {
+            System.out.print(entry.getKey() + "-" + entry.getValue() + " ");
+        }
+        System.out.println();
+
+        NavigableMap<Integer, String> ascendingMap = descendingMap.descendingMap();
+        Set<Map.Entry<Integer, String>> ascendingEntrySet = ascendingMap.entrySet();
+        for (Map.Entry<Integer, String> entry : ascendingEntrySet) {
+            System.out.print(entry.getKey() + "-" + entry.getValue() + " ");
+        }
+    }
+}
