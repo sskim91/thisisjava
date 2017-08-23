0f5fbaf05a070eb80ca373212ade7d3e225659e2
sskim <tjdtjq91@gmail.com>
2017-08-23 16:31:37 +0900

    Map 컬렉션 HashMap

---
+++ src/chapter15/Student.java
@@ -0,0 +1,28 @@
+package chapter15;
+
+public class Student {
+
+    //키로 사용할 객체 - hashCode()와 equals() 재정의
+    public int sno;
+    public String name;
+
+    public Student(int sno, String name) {
+        this.sno = sno;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Student) {
+            Student student = (Student) obj;
+            return (sno == student.sno) && (name.equals(student.name));
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return sno + name.hashCode();
+    }
+}
