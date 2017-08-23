69aa2bea9974e484c32eba6cbe1e5fb22b171d57
sskim <tjdtjq91@gmail.com>
2017-08-23 17:02:28 +0900

    TreeSet 이진트리

---
+++ src/chapter15/TreeSetExample3.java
@@ -0,0 +1,25 @@
+package chapter15;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSetExample3 {
+    public static void main(String[] args) {
+        //영어 단어를 정렬하고, 범위 검색해보기
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("apple");
+        treeSet.add("forever");
+        treeSet.add("description");
+        treeSet.add("ever");
+        treeSet.add("zoo");
+        treeSet.add("base");
+        treeSet.add("guess");
+        treeSet.add("cherry");
+
+        System.out.println("[c~f 사이의 단어 검색]");
+        NavigableSet<String> rangeSet = treeSet.subSet("c", true, "f", true);
+        for (String word : rangeSet) {
+            System.out.println(word);
+        }
+    }
+}
