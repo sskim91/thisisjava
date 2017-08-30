a0e573eb41fd99dee3cc7c477667dfa4c4e1c585
sskim <tjdtjq91@gmail.com>
2017-08-30 17:16:08 +0900

    리턴값이 있는 람다식 예제

--- src/chapter14/MyFunctionalInterfaceExample.java
+++ src/chapter14/MyFunctionalInterfaceExample.java
@@ -4,18 +4,23 @@ public class MyFunctionalInterfaceExample {
     public static void main(String[] args) {
         MyFunctionalInterface fi;
 
-        fi = (x) -> {
-            int result = x*5;
-            System.out.println(result);
+        fi = (x, y) -> {
+            int result = x+y;
+            return result;
         };
-        fi.method(2);
+        System.out.println(fi.method(2,5));
 
-        fi = (x) -> {
-            System.out.println(x*5);
-        };
-        fi.method(2);
+        fi = (x, y) -> { return x + y; };
+        System.out.println(fi.method(2,5));
 
-        fi = (x) -> System.out.println(x*5);
-        fi.method(2);
+        fi = (x, y) -> x + y;                   //return 문만 있을 경우 중괄호{}와 return문 생략 가능
+        System.out.println(fi.method(2,5));
+
+        fi = (x, y) -> sum(2, 5);
+        System.out.println(fi.method(2, 5));
+    }
+
+    public static int sum(int x, int y) {
+        return (x + y);
     }
 }
