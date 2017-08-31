9af6de0916a587045577d5fa743b7c1c63192897
sskim <tjdtjq91@gmail.com>
2017-08-31 20:44:44 +0900

    스트림 반복자 스트림 특징 예제

---
+++ src/chapter16/IteratorVsStreamExample.java
@@ -0,0 +1,27 @@
+package chapter16;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class IteratorVsStreamExample {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("홍길동", "김성섭", "김자바");
+
+        //Iterator이용
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        System.out.println();
+
+        //Stream 이용
+        Stream<String> stream = list.stream();
+        stream.forEach(name -> System.out.println(name));
+
+    }
+}
