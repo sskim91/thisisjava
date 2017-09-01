f8efdc8644d08e9324aad0342e65fd132c43ab77
sskim <tjdtjq91@gmail.com>
2017-09-01 19:19:42 +0900

    컬렉션과 배열로부터 스트림 얻기

---
+++ src/chapter16/FromArrayExample.java
@@ -0,0 +1,22 @@
+package chapter16;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FromArrayExample {
+    public static void main(String[] args) {
+
+        //배열로부터 스트림 얻기
+        String[] strArray = {"홍길동", "김성섭", "북카페"};
+        Stream<String> strStream = Arrays.stream(strArray);
+        strStream.forEach(s -> System.out.println(s + ","));
+
+        System.out.println();
+
+        int[] intArray = {1, 2, 3, 4, 5};
+        IntStream intStream = Arrays.stream(intArray);
+        intStream.forEach(s -> System.out.println(s + ","));
+
+    }
+}
---
+++ src/chapter16/FromIntRangeExample.java
@@ -0,0 +1,15 @@
+package chapter16;
+
+import java.util.stream.IntStream;
+
+public class FromIntRangeExample {
+
+    public static int sum;
+
+    public static void main(String[] args) {
+        //정수 범위 Stream
+        IntStream intStream = IntStream.range(1, 100);
+        intStream.forEach(s -> sum += s);
+        System.out.println(sum);
+    }
+}
